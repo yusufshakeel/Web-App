3965319f77becedb6dee50c68c71650c905d789d
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-01-29 02:50:16 +0530

    html5 analog clock with calendar

---
+++ analog-clock2/script.js
@@ -0,0 +1,64 @@
+function init(){
+    document.getElementById('loading').style.visibility="hidden";
+    document.getElementById('main-container').style.visibility="visible";
+    clock();
+};
+
+function clock(){
+    var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var month=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    //calculate angle
+    var d, h, m, s;
+    d = new Date;
+    
+    h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
+    m = 6 * d.getMinutes();
+    s = 6 * d.getSeconds();
+    
+    //move hands
+    setAttr('h-hand', h);
+    setAttr('m-hand', m);
+    setAttr('s-hand', s);
+    setAttr('s-tail', s+180);
+    
+    //display time
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    
+    if(h >= 12){
+        setText('suffix', 'PM');
+    }else{
+        setText('suffix', 'AM');
+    }
+    
+    if(h != 12){
+        h %= 12;
+    }
+    
+    setText('sec', s);
+    setText('min', m);
+    setText('hr', h);
+    
+    setText('day',day[d.getDay()]);
+    setText('date',d.getDate());
+    setText('month-year',month[d.getMonth()]+' '+(1900+d.getYear()));
+    
+    //call every second
+    setTimeout(clock, 1000);
+    
+};
+
+function setAttr(id,val){
+    var v = 'rotate(' + val + ', 70, 70)';
+    document.getElementById(id).setAttribute('transform', v);
+};
+
+function setText(id,val){
+    if(val < 10){
+        val = '0' + val;
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+window.onload=init;
