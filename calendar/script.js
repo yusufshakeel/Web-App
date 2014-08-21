99cae23efd3a58a012341e36e226dc73ff53f3bb
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-08-21 18:42:23 +0530

    calendar

--- calendar/script.js
+++ calendar/script.js
@@ -1,20 +1,21 @@
+//this function will find today's date
 function calendar(){
     var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var month=['January','February','March','April','May','June','July','August','September','October','November','December'];
-    var d = new Date();
-	setText('calendar-day',day[d.getDay()]);
-    setText('calendar-date',d.getDate());
-    setText('calendar-month-year',month[d.getMonth()]+' '+(1900+d.getYear()));
-    
-    //call every second
-    setTimeout(calendar, 1000);
+    var d=new Date();
+    setText('calendar-day', day[d.getDay()]);
+    setText('calendar-date', d.getDate());
+    setText('calendar-month-year', month[d.getMonth()]+' '+(1900+d.getYear()));
 };
 
-function setText(id,val){
+//this function will set the text value of <p> tags
+function setText(id, val){
     if(val < 10){
-        val = '0' + val;
+        val = '0' + val;    //add leading 0 if val < 10
     }
     document.getElementById(id).innerHTML = val;
 };
 
-window.onload=calendar;
+
+//call calendar() when page load
+window.onload = calendar;
