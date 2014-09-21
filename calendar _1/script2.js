b0281c6ed7e78f3eeb81be6f99bb375e23c43a1a
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-09-21 18:51:48 +0530

    initial commit

---
+++ calendar _1/script2.js
@@ -0,0 +1,26 @@
+//this function will find today's date
+function calendar(){
+    var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var month=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var d=new Date();
+    setText('calendar-day', day[d.getDay()]);
+    setText('calendar-date', d.getDate());
+    setText('calendar-month-year', month[d.getMonth()]+' '+(1900+d.getYear()));
+};
+
+//this function will set the text value of <p> tags
+function setText(id, val){
+    if(val < 10){
+        val = '0' + val;    //add leading 0 if val < 10
+    }
+    
+    var elements = document.getElementsByClassName(id);
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].innerHTML = val;
+    }
+};
+
+
+//call calendar() when page load
+window.onload = calendar;
